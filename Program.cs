f192672d2cbcd792e06ca5ebfb2c8102fa09f05e
Sadan Magar <sadanmagar789@gmail.com>
2022-07-11 08:21:00 +0545

    category added sucessfully

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ITestingService testingService = new TestingService();
+            testingService.Create();
+            testingService.Update();
+            testingService.Delete();
+            Console.ReadLine();
+        }
+    }
+
+    public interface ITestingService
+    {
+        void Create();
+        void Update();
+        void Delete();
+    }
+
+    public class TestingService : ITestingService
+    {
+        public void Create()
+        {
+            Console.WriteLine("catagory created successfully");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("catagory deleted successfully");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("catagory updated successfully");
+
+        }
+    }
+
+
+}
