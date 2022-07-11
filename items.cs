86c7b529c40c53b1019689916ee0cefae2d15903
sbn <mickeymouseef2@gmail.com>
2022-07-11 08:50:56 +0545

    Items added

---
+++ items.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @ItemsISMT
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ITestingItems testingItems = new TestingItems();
+            testingItems.Create();
+            testingItems.Update();
+            testingItems.Delete();
+            Console.ReadLine();
+        }
+    }
+
+    public interface ITestingItems
+    {
+        void Create();
+        void Update();
+        void Delete();
+    }
+
+    public class TestingItems : ITestingItems
+    {
+        public void Create()
+        {
+            Console.WriteLine(" Items created successfully");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Items Deleted successfully");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine(" Items Updated successfully");
+
+        }
+    }
+
+
+}
