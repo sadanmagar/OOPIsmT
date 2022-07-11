c8cf90fe07f94cfa42ee1e2b3780cac78c9e43ff
krishna <fuckboypawan@gmail.com>
2022-07-11 09:01:58 +0545

    File added succes

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello World");
+            Console.ReadLine();
+        }
+    }
+    public interface IEmployeeService
+    {
+        void add(Employee employee);
+        void delete(int Id);
+       
+    }
+    public class Employee : IEmployeeService
+    {
+        public void add(Employee employee)
+        {
+            
+        }
+
+        void IEmployeeService.delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
